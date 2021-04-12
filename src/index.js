6910406329e9b2f9f30f87038d67b5e83e3b4df6
Eero Lahdenperä <eero.lahdenpera@gmail.com>
2021-04-12 22:45:24 +0300

    Add procfile and dynamic port

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 import express from "express"
 const app = express()
-const port = 3000
+const port = process.env.PORT || 5000;
 
 app.get('/', async (req, res) => {
   const rank_list = await fetch();
