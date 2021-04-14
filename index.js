08d251f4c035f535b68afb86db1dd1f347d6ac0b
Eero Lahdenperä <eero.lahdenpera@gmail.com>
2021-04-14 20:37:25 +0300

    Small UI improvements

--- index.js
+++ index.js
@@ -3,29 +3,31 @@ import express from "express"
 const app = express()
 const port = process.env.PORT || 3000;
 
-app.get('/', async (req, res) => {
+
+const players = ["Kufdon", "DHawk", "Merza", "Alpha", "Sopamaa", "Agathanos", 
+"Vardu", "Zumpp", "kane8D", "Roarr", "Snurmi", "Habugabu", "Troni", "Pietzu10", 
+"Makkis", "Harzz", "Tume", "Drafkunex", "monki3", "majis1", "Threat", "Cpra", 
+"Bezikki", "FR33V", "Sepelaaja", "Wampie", "m4k3z"].join("|").toLowerCase().split("|")
+
+app.get('/', async (_req, res) => {
   const rank_list = await fetch();
-  // const rank_list = [{ accountid: "foo", rank: "10" }, { accountid: "bar", rank: "15" }]
-  res.render('index', { rank_list: rank_list })
+  // const rank_list = [{ accountid: "foo", rank: "10" }, { accountid: "bar", rank: "15" },
+  // { accountid: "kolmas", rank: "17" }, { accountid: "joku", rank: "77" }, { accountid: "qwerty", rank: "77" }]
+  res.render("index", { rank_list: rank_list })
+})
+
+app.get("/players", (_req, res) => {
+  res.render("players", { players: players })
 })
 
 const fetch = async () => {
-  const rank_list = []
-  const players = ["Kufdon", "DHawk", "Merza", "Alpha", "Sopamaa", "Agathanos", 
-  "Vardu", "Zumpp", "kane8D", "Roarr", "Snurmi", "Habugabu", "Troni", "Pietzu10", 
-  "Makkis", "Harzz", "Tume", "Drafkunex", "monki3", "majis1", "Threat", "Cpra", 
-  "Bezikki", "FR33V", "Sepelaaja", "Wampie", "m4k3z"].join("|").toLowerCase().split("|")
   const response = await axios.get("https://playhearthstone.com/en-gb/api/community/leaderboardsData?region=EU&leaderboardId=CLS")
   const rows = response.data.leaderboard.rows;
-  rows.forEach((e) => {
-    if (players.includes(e.accountid.toLowerCase())) {
-      rank_list.push(e)
-    }
-  })
-  return rank_list
+  return rows.filter((e) => players.includes(e.accountid.toLowerCase()))
 }
 
 app.set('view engine', 'pug')
+app.use('/favicon.ico', express.static('assets/favicon.png'));
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`)
 })
